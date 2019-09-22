cbb92746aba770186d5939d53de9a2956f62ce3b
Devon Knudsen <kingdevon12@gmail.com>
2019-09-21 19:49:08 -0500

    Added the ability to input any binary string directly from the terminal as well as some refined comments, team name and repo URL

--- BinaryDecoder.py
+++ BinaryDecoder.py
@@ -1,56 +1,62 @@
+# Persians: Sydney Anderson, Tram Doan, Devon Knudsen, Zackary Phillips, Promyse Ward, James Wilson
+# GitHub Repo: https://github.com/devonknudsen/Binary-Decoder
 # Written in Python 3.7.
-#111010111100111100101111001001110101110010110111111011111110100010000011100001100001111001111100110111010110000111100111110100111001011011111101110110111111011011111001000100011001011110010
-import sys
 
 # converts a binary string into a character
 def binaryToChar(b):
     asc = int(b,2)
+
     #check for backspace
     if(asc == 8):
         return -1
     char = chr(asc)
+
     return char
 
 def bit7(binaryString):
     # splits the binary string up into groups of 7
     binaryArr = [binaryString[i:i+7] for i in range(0, len(binaryString), 7)]
+
     charString = ""
     
     # converts each binary string to a character
-    # and adds it to a string
+    # and appends it to a string
     for i in range(0, int(len(binaryString)/7)):
         charCheck = binaryToChar(binaryArr[i])
-        #this will remove the char at the end 
+
         if(charCheck != -1):
             charString += charCheck
         else:
             charString = charString[:-1]
+
     return charString
     
 def bit8(binaryString):
     # splits the binaryString string up into groups of 8
     binaryArr = [binaryString[i:i+8] for i in range(0, len(binaryString), 8)]
+
     charString = ""
     
     # converts each binary string to a character
-    # and adds it to a string
+    # and appends it to a string
     for i in range(0, int(len(binaryString)/8)): 
         charCheck = binaryToChar(binaryArr[i])
-        #this will remove the char at the end
+
         if(charCheck != -1):
             charString += charCheck
         else:
             charString = charString[:-1]
+
     return charString
 
-# Main Code
-f = sys.stdin.read()
+# MAIN
+f = input()
 
-# 7 bit
+# 7 bit Binary
 if (len(f) % 7 == 0):
     print(bit7(f))
     
-# 8 bit
+# 8 bit Binary
 if (len(f) % 8 == 0):
     print(bit8(f))
     
